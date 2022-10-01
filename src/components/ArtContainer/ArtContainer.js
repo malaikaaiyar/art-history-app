bc5b066e0250f86c6ae0458199bb449294665de3
malaika <51083763+malaikaaiyar@users.noreply.github.com>
2022-10-01 12:27:26 +0100

    fix the thing

--- src/components/ArtContainer/ArtContainer.js
+++ src/components/ArtContainer/ArtContainer.js
@@ -6,12 +6,16 @@ function ArtContainer({ data, currentYear }) {
 
     console.log(currentYear)
     console.log("filtered", filteredData)
+
+    const fixUrl = (url) => {
+        return url.slice(0,7) + 'media' + url.slice(10, -6) + "_10.jpg"
+    }
     
     return <>
         <div className="container">
             <div className="cards">
                 {filteredData.map((piece) => {
-                    return <ArtModal imgUrl={piece.data_thumbnail} title={piece.data_title} artist={piece.artist_name} medium={piece.data_medium}/>
+                    return <ArtModal imgUrl={fixUrl(piece.data_thumbnail)} title={piece.data_title} artist={piece.artist_name} medium={piece.data_medium}/>
                 })}   
                 {filteredData.length === 0 && <p>No art for this year yet!</p>}
             </div>
